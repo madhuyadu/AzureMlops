0dbdfab273f0fd241b6c8b84a86f00b6b410b414
Madhusudhan B <56335301+madhuyadu@users.noreply.github.com>
2023-04-27 11:05:38 +0530

    Update hello.py

--- src/model/hello.py
+++ src/model/hello.py
@@ -1,3 +1,4 @@
 print("sample file created to check pull request for main branch")
 print("sample file created to check pull request for main branch")
 print("sample file created to check pull request for main branch")
+print("test comment for pulling into locally cloned repo")
